37260bb1fc017f70a333367868a002d63cb68f6d
luk <mail-login+gitlab@protonmail.com>
2021-06-06 22:12:47 +0200

    Add examples to config.py #16

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import re
 import math
 from typing import Callable, Iterable
 import random
+import itertools
 
 from pyformlang.cfg import CFG
 # only in effect if --check is set
@@ -10,31 +11,47 @@ from pyformlang.cfg import CFG
 # TIPP: if you'd like to hardcode some words, use a dictionary here, e.g. ret {'aba':True, 'aab':False}
 # Things like the commented use of an other CFG to use as check are possible too!
 
-# cfg = None # global variable sp that the cfg doesn't have to be read all the time
-def checkL(i:str):
-    # global cfg
-    # if cfg is None: # initialize cfg if not alread done
-    # # productions written as in yaml, prods separated by newline (not in a list like in yaml)
-    #     cfg = CFG.from_text("""
-    #             S -> S S | a S S b | $
-    #         """)
+def abEx(i:str):
     ret = len(re.findall("ab", i)) == len(re.findall('ba', i))
-    # ret = cfg.contains(i)
     return ret
+
+cfg = CFG.from_text("S -> a b")
+def cfgContain(i:str):
+    return cfg.contains(i)
+
+def checkL(i:str):
+    return abEx(i)
 
 # only in effect if input is generated randomly
 # determines how many words should be tested for a given length
 # maybe you'd like to choose a linear or a quadratic function
 def cntPerLength(l:int) -> int:
+    return cntAll(l)
+    # return cntRand(l)
+
+def cntAll(l:int) -> int:
+    return 3**l
+
+def cntRand(l:int) -> int:
     return l+1
 
 # only in effect, if no checks argument is given in the input yaml
 # genrates the words that are checked. This is provided, since sometimes the
 # default random generator isn't good enough and only wrong words are checked
 def genRandomWords(startLen:int, endLen:int, cntPerLength:Callable[[int],int], terminals:list[str]) -> Iterable[str]:
+    # return genAll(startLen, endLen, terminals)
+    return genRand(startLen, endLen, cntPerLength, terminals)
+
+def genAll(startLen:int, endLen:int, terminals:list[str]) -> Iterable[str]:
+    for l in range(startLen,endLen):
+        rs = itertools.product(*tuple([terminals])*l)
+        for r in rs:
+            yield "".join(r)
+
+def genRand(startLen:int, endLen:int, cntPerLength:Callable[[int],int], terminals:list[str]) -> Iterable[str]:
     for l in range(startLen, endLen):
         for _ in range(cntPerLength(l)):
-            string = ""
+            s = ""
             for _ in range(l):
-                string += random.choice(terminals)
-            yield string
+                s += random.choice(terminals)
+            yield s
